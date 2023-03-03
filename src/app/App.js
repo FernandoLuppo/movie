fe6f8bf7a17cb2366d892162ff314ceeaefb064a
Fernando José Alves Luppo <fernandoluppo2@gmail.com>
2023-03-03 09:52:34 -0300

    [02] make a new build

--- src/app/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-const routes_1 = require("./routes/routes");
-const App = () => {
-    return <routes_1.Routes />;
-};
-exports.App = App;
