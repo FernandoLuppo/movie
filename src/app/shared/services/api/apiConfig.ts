65472748c873bc8cf290b3a380e7218c4870944e
Fernando José Alves Luppo <fernandoluppo2@gmail.com>
2023-03-03 11:05:11 -0300

    [03] Change api url

--- src/app/shared/services/api/apiConfig.ts
+++ src/app/shared/services/api/apiConfig.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export const Api = () => {
     return axios.create({
-        baseURL: "https://technotes-api.onrender.com"
+        baseURL: "https://luppotw-movies-wxm4.onrender.com"
     })
 }
