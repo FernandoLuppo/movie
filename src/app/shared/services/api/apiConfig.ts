0579bc1cdc62e1df506b40974b94f4152043e891
Fernando José Alves Luppo <fernandoluppo2@gmail.com>
2023-03-07 20:19:30 -0300

    [08] change axios base url

--- src/app/shared/services/api/apiConfig.ts
+++ src/app/shared/services/api/apiConfig.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const productionURL = "https://luppotw-movies-api.onrender.com";
-const localURL = "http://localhost:3001";
-
 export const Api = () => {
   return axios.create({
-    baseURL: productionURL,
+    baseURL: "https://luppotw-movies-api.onrender.com",
   });
 };
