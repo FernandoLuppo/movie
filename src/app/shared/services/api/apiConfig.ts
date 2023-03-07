965bd9fa2c4c3d83c643b76cf9583ca5f939f037
Fernando José Alves Luppo <fernandoluppo2@gmail.com>
2023-03-07 19:53:54 -0300

    [07] bugfix layout login/register

--- src/app/shared/services/api/apiConfig.ts
+++ src/app/shared/services/api/apiConfig.ts
@@ -1,7 +1,10 @@
-import axios from "axios"
+import axios from "axios";
+
+const productionURL = "https://luppotw-movies-api.onrender.com";
+const localURL = "http://localhost:3001";
 
 export const Api = () => {
-    return axios.create({
-        baseURL: "https://luppotw-movies-api.onrender.com"
-    })
-}
+  return axios.create({
+    baseURL: productionURL,
+  });
+};
